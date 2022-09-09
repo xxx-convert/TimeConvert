760737ba11f3fcf29b93f6c48e849b7d1a798871
baishanglin001 <baishanglin001@ke.com>
2022-09-09 22:30:07 +0800

    [feature] add StopWatch and unittest * 增加StopWatch功能，方便记录代码运行时间 * 仅支持python version >=3.6.*

---
+++ tests/test_stop_watch.py
@@ -0,0 +1,24 @@
+import time
+
+from TimeConvert import StopWatch, TimeType
+
+
+class TestStopWatch(object):
+
+    # Variable
+
+    def test_stop_watch(self):
+        sw = StopWatch('我是一个计时器', time_type=TimeType.second)
+        sw.start('我是计时点1')
+        time.sleep(0.01)
+        # 计时点1计时结束
+        sw.stop()
+
+        sw.start('我是计时点2')
+        time.sleep(0.02)
+        # 计时点2计时结束
+        sw.stop()
+        sw.pretty_print()
+
+
+
